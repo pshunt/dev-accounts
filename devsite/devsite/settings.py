ee92d00d91b659aa2c73a17b57f8e61efa8a186d
pshunt <peter.s.hunt@gmail.com>
2025-10-09 23:18:43 -0700

    update settings

--- devsite/devsite/settings.py
+++ devsite/devsite/settings.py
@@ -89,14 +89,12 @@ WSGI_APPLICATION = 'devsite.wsgi.application'
 
 # Postgres Section
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
 DEBUG_PROPAGATE_EXCEPTIONS = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 
