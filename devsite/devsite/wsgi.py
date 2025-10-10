7a473abcfca1731a7e49ebfa7309b6e04172a239
pshunt <peter.s.hunt@gmail.com>
2025-10-10 08:09:05 -0700

    update project wsgi path

--- devsite/devsite/wsgi.py
+++ devsite/devsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv("DJANGO_SETTINGS_MOD"))
 
 application = get_wsgi_application()
